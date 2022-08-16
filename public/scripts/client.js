00045f225ec4635842ecebb0235bbe8421d10f3a
Eds-Dbug <edsdbug@gmail.com>
2022-08-16 20:47:22 +0000

    created date converter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,26 +8,32 @@ const data = [
   {
     "user": {
       "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
+      "avatars": "https://i.imgur.com/73hZDYK.png",
       "handle": "@SirIsaac"
     },
     "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-    "created_at": 1461116232227
+    "created_at": 1660503656377
   },
   {
     "user": {
       "name": "Descartes",
       "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
+      "handle": "@rd"
+    },
     "content": {
       "text": "Je pense , donc je suis"
     },
-    "created_at": 1461113959088
+    "created_at": 1660590056377
   }
 ]
+
+const convertDate = function(date) {
+  const resultDate = new Date(date);
+  const resultArr = String(resultDate).split(' ');
+  return `${resultArr[0]} ${resultArr[1]} ${resultArr[2]} ${resultArr[3]}`
+}
 
 const renderTweets = function(tweets) {
   // loops through tweets
@@ -45,14 +51,14 @@ function createTweetElement(tweetData) {
   let $tweet = (`<article class="tweet">
     <header>
       <div>
-        <img class="logo" src=${tweetData.avatars} width="40" height="40">
+        <img class="logo" src=${tweetData.user.avatars} width="40" height="40">
         <span class="personName">${tweetData.user.name}</span>
       </div>
       <span>${tweetData.user.handle}</span>
     </header>
     <p>${tweetData.content.text}</p>
     <footer>
-      <span>${tweetData.created_at}</span>
+      <span>${convertDate(tweetData.created_at)}</span>
       <div class="options">
         <i class="fa fa-flag" aria-hidden="true"></i>
         <i class="fa fa-retweet" aria-hidden="true"></i>
@@ -64,16 +70,15 @@ function createTweetElement(tweetData) {
   return $tweet;
 }
 
-renderTweets(data); 
-
-
-
-
-
-
-
 // Test / driver code (temporary)
 $(document).ready(function() {
-  renderTweets(data)
- 
+  renderTweets(data);
+  
+  //console.log(initialTweets);
+
+  $createTweet = $('#tweet-form');
+  $createTweet.submit(function(e) {
+    
+    e.preventDefault();
+  })
 })
