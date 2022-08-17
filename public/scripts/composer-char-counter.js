23c947ec9eadf3e5fe82258e3923be90611cc81e
Eds-Dbug <edsdbug@gmail.com>
2022-08-17 23:46:52 +0000

    testing refactor on master

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
 $(document).ready( function() {
-  //console.log('hello there');
+  alert('on master');
 
   $("#tweet-text").on('input', function() {
     let $counterElement = $(this).parent().children('.btn-counter').children('.counter');
