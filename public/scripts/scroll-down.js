b8a4ae83cc6259d6a1779d6e3b904b61fe9b3e78
Eds-Dbug <edsdbug@gmail.com>
2022-08-21 00:09:16 +0000

    implement behaviour to scroll to top with togg;e-top button.

---
+++ public/scripts/scroll-down.js
@@ -0,0 +1,15 @@
+const $toggleTop = $('.toggle-top');
+
+$(document).scroll(function () {
+  const fromTop = $(this).scrollTop();
+  if(fromTop > 50) {
+    $toggleTop.show();
+  } else {
+    $toggleTop.hide();
+  }
+});
+
+$toggleTop.click(function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+})
+
